e83203cdd0a4e8c8f9aa2a07ba2682ec8318f210
ankitmalhotra94 <ankit14894@gmail.com>
2022-11-05 01:37:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import os
 
 st.set_page_config(page_title="Login", page_icon=":health:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 if 'login' not in st.session_state:
         st.session_state['login'] = False
